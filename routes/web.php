4d98eaa01f2852844ad51e145941cb5df2dfd118
Sian Nun <nunsay872@gmail.com>
2025-06-19 15:46:37 +0700

    Add temporary migration route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
-
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/migrate', function () {
+    Artisan::call('migrate', ['--force' => true]);
+    return 'Migrations have been run.';
 });
